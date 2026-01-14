383a7c838d5edf2bb6406fc931a090e4d21c859f
Hassan Muhayyudin <muhayyudinhassan@gmail.com>
2026-01-14 11:14:57 +0500

    Initial commit

--- app.js
+++ app.js
@@ -24,16 +24,20 @@ buttons.forEach((button) => {
 document.addEventListener("keydown", (e) => {
   const key = e.key;
   if ((e.key >= "0" && e.key <= "9") || ["+", "-", "*", "/"].includes(e.key)) {
+    e.preventDefault();
     display.value += key;
   } else if (key === "Enter") {
     try {
+      e.preventDefault();
       display.value = math.evaluate(display.value);
     } catch (error) {
       display.value = "ERROR";
     }
   } else if (key === "Backspace") {
+    e.preventDefault();
     display.value = display.value.slice(0, -1);
-  } else if (key === "Escape" || key === "C") {
+  } else if (key === "Escape" || key.toUpperCase() === "C") {
+    e.preventDefault();
     display.value = "";
   }
 });
